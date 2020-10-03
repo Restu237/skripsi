c9cc52c0b07af97422f26c84f73b58073bf51ff1
Restu237 <restupujilestari23@gmail.com>
2020-10-03 17:21:50 +0700

    fix database on restu

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+//use Illuminate\Support\Facedes\Schema;
+use Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+    
+        //fix error migrate
+        Schema::defaultStringLength(191);
     }
 }
